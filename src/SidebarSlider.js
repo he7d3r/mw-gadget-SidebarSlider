d1bcf7d29a821076e36a1bbbae395d7f3cdb1caf
Helder <he7d3r@gmail.com>
2011-12-14 16:13:56 -0200

    Invert the pattern

--- src/SidebarSlider.js
+++ src/SidebarSlider.js
@@ -15,7 +15,7 @@ $(function(){
 		$('#left-navigation').animate( {'left' : '1em'} );
 		$('#content,#footer').animate( {'marginLeft' : '1em'} );
 	}
-
+ 
 	window.iPadSidebarCustom_in = function(){
 		$('#mw-panel').animate( {width: 'show'} );
 		$('#left-navigation').animate( {'left' : '10em'} );
@@ -26,19 +26,19 @@ $(function(){
 	//$('#mw-panel').hide();
 	//$('#left-navigation').css( 'left', '1em' );
 	//$('#content,#footer').css( 'marginLeft', '1em' );
-	$('body').append( '<div id="kr-panel-toggle" style="width:28px;height:28px;background:white url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png) center no-repeat;border:2px solid #f3f3f3;position:fixed;left:-5px;top:30%;-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px"></div>' );
+	$('body').append( '<div id="kr-panel-toggle" style="width:28px;height:28px;background:white url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png) center no-repeat;border:2px solid #f3f3f3;position:fixed;left:9em;top:30%;-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px"></div>' );
 	$('#kr-panel-toggle').hover( function(){
 		$(this).css( 'cursor', 'pointer' );
 	}, function(){
 		$(this).css( 'cursor', 'default' );
 	});
 	$('#kr-panel-toggle').click( function(){
-		if ( $(this).hasClass( 'go-out' ) ){
-			iPadSidebarCustom_out();
-			$(this).removeClass( 'go-out' ).addClass( 'go-in' ).animate( {'left' : '-5px'} ).css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png)') ;
-		} else {
+		if ( $(this).hasClass( 'go-in' ) ){
 			iPadSidebarCustom_in();
 			$(this).removeClass( 'go-in' ).addClass( 'go-out' ).animate( {'left' : '9em'} ).css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png)' );
+		} else {
+			iPadSidebarCustom_out();
+			$(this).removeClass( 'go-out' ).addClass( 'go-in' ).animate( {'left' : '-5px'} ).css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png)') ;
     }
   });
 
