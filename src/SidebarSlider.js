8f561b11a1501bba76f176df258031e9ecbdf21e
Helder <he7d3r@gmail.com>
2014-09-26 15:21:26 -0300

    Define JSHint configuration on .jshintrc
    Rename two variables to use camelCase.

--- src/SidebarSlider.js
+++ src/SidebarSlider.js
@@ -8,8 +8,6 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @source: [[commons:MediaWiki:IPadSidbarSlider.js]]
  */
-/*jslint browser: true, white: true*/
-/*global jQuery, mediaWiki */
 ( function ( $ ) {
 'use strict';
 
@@ -17,11 +15,11 @@ $(function(){
 
 	//if (navigator.userAgent.match(/iPad/i) == null) return; //Dont ignore non-iPads (yet)
 
-	var iPadSidebarCustom_out = function(){
+	var iPadSidebarCustomOut = function(){
 		$('#mw-panel').animate( {width: 'hide'} );
 		$('#left-navigation').animate( {'left' : '1em'} );
 		$('#content,#footer').animate( {'marginLeft' : '1em'} );
-	}, iPadSidebarCustom_in = function(){
+	}, iPadSidebarCustomIn = function(){
 		$('#mw-panel').animate( {width: 'show'} );
 		$('#left-navigation').animate( {'left' : '10em'} );
 		$('#content,#footer').animate( {'marginLeft' : '10em'} );
@@ -39,11 +37,11 @@ $(function(){
 	});
 	$('#kr-panel-toggle').click( function(){
 		if ( $(this).hasClass( 'go-in' ) ){
-			iPadSidebarCustom_in();
+			iPadSidebarCustomIn();
 			$(this).removeClass( 'go-in' ).addClass( 'go-out' ).animate( {'left' : '9em'} )
 				.css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png)' );
 		} else {
-			iPadSidebarCustom_out();
+			iPadSidebarCustomOut();
 			$(this).removeClass( 'go-out' ).addClass( 'go-in' ).animate( {'left' : '-5px'} )
 				.css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png)') ;
 		}
