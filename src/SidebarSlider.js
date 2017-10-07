fbd9f7d05226b5736d45821681b43bcf369bd5ae
Helder <he7d3r@gmail.com>
2017-10-07 14:40:05 -0300

    Replace ".hover(...)" by ".on('mouseenter', ...)"
    Copy from https://commons.wikimedia.org/w/index.php?title=MediaWiki:IPadSidbarSlider.js&oldid=261874015

--- src/SidebarSlider.js
+++ src/SidebarSlider.js
@@ -2,50 +2,64 @@
  * iPad Sidebar Slider
  * Created on June 16th, 2010
  *
- * @rev 15 (2011-10-16)
+ * @rev 16 (2017-10-06)
  * @author: Krinkle
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @source: [[commons:MediaWiki:IPadSidbarSlider.js]]
+ * @source: [[c:MediaWiki:IPadSidbarSlider.js]]
+ * @see: [[c:MediaWiki:Adjust4iPad.js]]
+ * @see: https://phabricator.wikimedia.org/T16501
  */
-( function ( $ ) {
-	'use strict';
+$(function () {
+// Dont ignore non-iPads (yet)
+// if (navigator.userAgent.match(/iPad/i) == null) return;
 
-	$(function () {
-
-		//if (navigator.userAgent.match(/iPad/i) == null) return; //Dont ignore non-iPads (yet)
-
-		var iPadSidebarCustomOut = function () {
-			$('#mw-panel').animate( { width: 'hide' } );
-			$('#left-navigation').animate( { 'left': '1em' } );
-			$('#content,#footer').animate( { 'marginLeft': '1em' } );
-		}, iPadSidebarCustomIn = function () {
-			$('#mw-panel').animate( { width: 'show' } );
-			$('#left-navigation').animate( { 'left': '10em' } );
-			$('#content,#footer').animate( { 'marginLeft': '10em' } );
-		};
-
-		//Initalize
-		//$('#mw-panel').hide();
-		//$('#left-navigation').css( 'left', '1em' );
-		//$('#content,#footer').css( 'marginLeft', '1em' );
-		$('body').append( '<div id="kr-panel-toggle" style="width:28px;height:28px;background:white url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png) center no-repeat;border:2px solid #f3f3f3;position:fixed;left:9em;top:30%;-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px"></div>' );
-		$('#kr-panel-toggle').hover( function () {
-			$(this).css( 'cursor', 'pointer' );
-		}, function () {
-			$(this).css( 'cursor', 'default' );
-		});
-		$('#kr-panel-toggle').click( function () {
-			if ( $(this).hasClass( 'go-in' ) ) {
-				iPadSidebarCustomIn();
-				$(this).removeClass( 'go-in' ).addClass( 'go-out' ).animate( { 'left': '9em' } )
-					.css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png)' );
-			} else {
-				iPadSidebarCustomOut();
-				$(this).removeClass( 'go-out' ).addClass( 'go-in' ).animate( { 'left': '-5px' } )
-					.css( 'background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png)') ;
-			}
-		});
+window.iPadSidebarCustomOut = function () {
+	$('#mw-panel').animate({
+		width: 'hide'
 	});
+	$('#left-navigation').animate({
+		'left': '1em'
+	});
+	$('#content,#footer').animate({
+		'marginLeft': '1em'
+	});
+};
 
-}( jQuery ) );
+window.iPadSidebarCustomIn = function () {
+	$('#mw-panel').animate({
+		width: 'show'
+	});
+	$('#left-navigation').animate({
+		'left': '10em'
+	});
+	$('#content,#footer').animate({
+		'marginLeft': '10em'
+	});
+};
+
+//Initalize
+$('#mw-panel').hide();
+$('#left-navigation').css('left', '1em');
+$('#content,#footer').css('marginLeft', '1em');
+$('body').append('<div id="kr-panel-toggle" style="width:28px;height:28px;background:white url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png) center no-repeat;border:2px solid #f3f3f3;position:fixed;left:-5px;top:30%;-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px"></div>');
+$('#kr-panel-toggle').on('mouseenter', function () {
+	$(this).css('cursor', 'pointer');
+}).on('mouseleave', function () {
+	$(this).css('cursor', 'default');
+});
+$('#kr-panel-toggle').click(function () {
+	if ($(this).hasClass('go-out')) {
+		iPadSidebarCustomOut();
+		$(this).removeClass('go-out').addClass('go-in').animate({
+			'left': '-5px'
+		}).css('background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Nuvola_single_chevron_right.svg/21px-Nuvola_single_chevron_right.svg.png)');
+	} else {
+		iPadSidebarCustomIn();
+		$(this).removeClass('go-in').addClass('go-out').animate({
+			'left': '9em'
+		}).css('background-image', 'url(//upload.wikimedia.org/wikipedia/commons/thumb/8/8e/1leftarrow.png/21px-1leftarrow.png)');
+	}
+});
+
+});
